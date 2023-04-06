1dfc98e862b8998bd9e60964ce1c86b0bc5bb2b2
LeopoldoIII <nathan.rx@gmail.com>
2023-04-06 17:20:21 -0600

    2 and 3

---
+++ 02-data-types.js
@@ -0,0 +1,8 @@
+const palindrome = "tacocat";
+const occupation = "Hand Model";
+const password = "t3st";
+
+const apy = 1.5;
+const numbersOfConcerts = 1000;
+
+const isOlderThan21 = false; 
---
+++ 03-arrays.js
@@ -0,0 +1,17 @@
+const favoriteCookies = [
+    "chocolate chip",
+    "iced oatmeal",
+    "sugar cookie"
+];
+
+favoriteCookies.push("windmill cookies");
+let cookie = favoriteCookies.pop();
+console.log(favoriteCookies);
+console.log("I am so sad because I'm gone - " + cookie);
+
+let fav = 2;
+console.log(favoriteCookies[fav]);
+
+console.log(favoriteCookies.length);
+console.log(favoriteCookies[favoriteCookies.length-1]);
+
