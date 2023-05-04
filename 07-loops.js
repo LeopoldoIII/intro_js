a3b666b8da183762ad5ec847e2f6ecf9353ee22f
LeopoldoIII <nathan.rx@gmail.com>
2023-05-04 14:11:00 -0600

    loops

---
+++ 07-loops.js
@@ -0,0 +1,14 @@
+
+const names = ["Mark", "Sarah", "Ebony"];
+
+for (let i =0; i < names.length; i++) {
+
+    console.log(names[i]);
+
+}
+
+const numbers = [1,2,3,4];
+
+for (const element of numbers) {
+    console.log(element * 2);
+}
---
+++ 08-while-loops.js
@@ -0,0 +1,26 @@
+
+let factorial = 1;
+let number = 5;
+let original = number; 
+
+do {
+    factorial = factorial * number;
+    number --
+    console.log(factorial);
+} while (number > 0);
+
+console.log(original + " factorial is " + factorial);
+
+
+
+let password = "password";
+let userGuess = "";
+
+// while (userGuess != password) {
+
+//     userGuess = prompt("Please enter your password");
+     
+// } 
+
+// alert("Login Succesful");
+
